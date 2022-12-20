489ab6a05772d9af498e616d81bffc683156fafe
multikirin <100300229+multikirin@users.noreply.github.com>
2022-12-20 18:19:02 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -182,7 +182,7 @@ spawn(function()
                 if v~= game:GetService("Players").LocalPlayer and not v.Character:FindFirstChildOfClass("ForceField") and v.Character.Humanoid.Health > 0 then
                     while v.Character:WaitForChild("Humanoid").Health > 0 do
                         wait();
-                        --game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = v.Character.HumanoidRootPart.CFrame;
+                        
                         for x, c in next, game:GetService("Players"):GetChildren() do
                             if c ~= game:GetService("Players").LocalPlayer then game.ReplicatedStorage.meleeEvent:FireServer(c) end
                         end
