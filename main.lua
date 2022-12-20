7268d5daba37556d7ef9da4bf8357aea57cff53d
multikirin <100300229+multikirin@users.noreply.github.com>
2022-12-20 18:21:38 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -172,6 +172,29 @@ end)
 end,
 })
 
+local prisonlifekillaura = Tab2:CreateButton({
+	Name = "Kill Aura",
+	Callback = function()
+spawn(function()
+    while wait(0.1) do
+        for i, v in next, game:GetService("Players"):GetChildren() do
+            pcall(function()
+                if v~= game:GetService("Players").LocalPlayer and not v.Character:FindFirstChildOfClass("ForceField") and v.Character.Humanoid.Health > 0 then
+                    while v.Character:WaitForChild("Humanoid").Health > 0 do
+                        wait();
+                        
+                        for x, c in next, game:GetService("Players"):GetChildren() do
+                            if c ~= game:GetService("Players").LocalPlayer then game.ReplicatedStorage.meleeEvent:FireServer(c) end
+                        end
+                    end
+                end
+            end)
+            wait()
+        end
+   end
+end)
+			end,
+})
 local prisonlifekillall = Tab2:CreateButton({
 	Name = "Kill All",
 	Callback = function()
@@ -182,7 +205,7 @@ spawn(function()
                 if v~= game:GetService("Players").LocalPlayer and not v.Character:FindFirstChildOfClass("ForceField") and v.Character.Humanoid.Health > 0 then
                     while v.Character:WaitForChild("Humanoid").Health > 0 do
                         wait();
-                        
+                        game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = v.Character.HumanoidRootPart.CFrame;
                         for x, c in next, game:GetService("Players"):GetChildren() do
                             if c ~= game:GetService("Players").LocalPlayer then game.ReplicatedStorage.meleeEvent:FireServer(c) end
                         end
