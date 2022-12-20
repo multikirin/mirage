99189c09858f7985eeb6c94006b3e4519acf95dd
multikirin <100300229+multikirin@users.noreply.github.com>
2022-12-20 17:55:32 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -180,7 +180,7 @@ local Slider2 = Tab:CreateSlider({
 	Flag = "speedspeed", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
 	Callback = function(Value)
 		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed=Value
-	end)}
+	end})
 
 --[[
 local Toggle = Tab:CreateToggle({
